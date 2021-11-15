29dd469e2fabba810a0c9ef6ba1b77d06299cba8
shafayet133st <82628454+shafayet133st@users.noreply.github.com>
2021-11-15 22:56:53 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+  <title>S@yed Shafayet Hossain</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+  <link rel="stylesheet"  type="text/css"href="css/style.php">
+    <link
+      href="https://fonts.googleapis.com/css?family=Poppins:300,900&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="css/main.css" />
+  </head>
+
+  <body>
+    <header>
+<nav class="navbar navbar-expand-md    sticky-top ">
+
+<a class="navbar-brand" href="index.php"><img src="st/972406052g3b4f.jpg" alt=""  width="50px" height="50px" /></a>
+<button class="navbar-toggler bg-white mr-5" type="button " data-toggle="collapse" data-target="#navbarResponsive">
+ <span class="navbar-toggler-icon text-info">M</span>
+</button>
+
+<div class=" collapse navbar-collapse  " id="navbarResponsive">
+<ul class="navbar-nav  nav-tabs  ml-auto">
+   <li class="nav-item  active">
+      <a class="nav-link text-white" href="index.php">HOME</a>
+    </li>
+    <li class="nav-item ">
+      <a class="nav-link  text-white"href="#portfolio">PORTFOLIO</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link  text-white" href="#skills&experience">skills & experience</a>
+    </li>
+	 <li class="nav-item">
+      <a class="nav-link  text-white" href="#mywork">my work</a>
+    </li>
+	<li class="nav-item">
+      <a class="nav-link  text-white" href="#about">About</a>
+    </li>
+	<li class="nav-item">
+      <a class="nav-link text-white" href="#con">contact</a>
+    </li>
+  </ul>
+   </div>
+ 
+ </nav>
+    </header>
+
+    <main>
+      <section class="section home-intro">
+        <div class="pic" >
+       <p>Hi</p>
+        <p>I'm S@yed shafayet hossain</p>
+        <p> WEB DESIGNER &  DEVELOPER</p>
+		<p>video + photo Editor</p>
+		<a href="#con"><button class="but">Hire Me</button></a><a href="shafayet.pdf" download><button class="but">Download My CV</button></a>
+  </div>
+     <div class="video-container">
+            <div class="color-overlay"></div>
+            <video autoplay loop muted vb>
+                <source src="videos.mp4" type="video/mp4">
+            </video>
+        </div>
+      </section>
+
+    <div class="  mb-5" id="portfolio"></div>
+<?php include"pages/pro.php"; ?>
+<div id="skills&experience"></div>
+<br />
+<?php include"pages/se.php"; ?>
+  <div class="  mb-5" id="mywork"></div>
+  <br />
+
+	<?php include"pages/work.php"; ?>
+	
+	
+   <div class="  mb-5" id="about"></div>
+   <br />
+  <?php include"pages/sid.php"; ?>
+   <div class="container-fluid bb text-white " >
+ <div class="container  " >
+ <div class="col-12"><h1 class="display-4 text-center"><u>About Me</u></h1></div>
+ <div class="row" >
+    <div class="col-sm-4">
+<h1>I believe in, the only  way to success is  time punctuality, hard working and honesty.  </h1>
+ </div>
+   <div class="col-sm-8">
+<h4>In student life I was a tutor. I was starting my internship before I graduated. At that time, our unit did not accept the internship. We only have alternative projects. After I finished my graduation I had started working in EduLeam.In EduLeam, i was collect data from Scholars' School and College and make a digital from and give it to programar. sometimes had to  supported the coustomar executive . i am also did  the sametype for work for BCIC  college and Bangladesh Navy (BN college).  I quit my job for family problems.I want to be a good web designer and developer so I started learning from Deepti and YouTube. </h4>
+ </div>
+  </div ></div> 
+  <div class="  mb-5" id="con"></div>
+   <br />
+  </div>
+  
+   <div class="container-fluid bb1 " >
+      <div class="container text-white ">
+ <div class="col-12"><h1 class="display-4 text-center "><u>CONTACT</u></h1></div>
+<div class="row">
+<div class="col-sm-4">
+<pre>
+<h5 class=" font-weight-bold text-white">
+Name:-S@yed shafayet Hossain
+
+Mobile Number:-01670891760
+
+
+Email:--Shafayet133@gmail.com
+
+Address:-15/1 Kingsuk tower,
+pallabi,Mirpur-1216,
+Dhaka,Bangladesh
+</h5>
+</pre>
+</div>
+<div class="col-sm-6 mb-5">
+<h2>Stacked form</h2>
+  <form action="/action_page.php">
+   <div class="form-group">
+      <label for="email">NAME</label>
+      <input type="email" class="form-control" id="name" placeholder="Name" name="name">
+    </div>
+    <div class="form-group">
+      <label for="email">Email:</label>
+      <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+    </div>
+	<div class="form-group">
+      <label for="email">Mobile Number:</label>
+      <input type="number" class="form-control" id="number" placeholder="Mobile number" name="number">
+    </div>
+    <div class="form-group">
+      <label for="commant text-uppercase">Commant</label>
+	 
+      <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    </div>
+
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+</div>
+</div>
+</div>
+</div>
+
+ <div class="container-fluid mt-5">
+ <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d542.5608726791809!2d90.36364671880365!3d23.822156778819938!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c12532d68c47%3A0x93087b262b5c9579!2zMjPCsDQ5JzIwLjQiTiA5MMKwMjEnNDkuNyJF!5e0!3m2!1sbn!2sbd!4v1564583651762!5m2!1sbn!2sbd" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+ </div>
+   <div class="container-fluid bg-success ">
+  <div class="container  ">
+ <div class="text-center text-white"> <h5>Power by &#169; S@yed shafayet hossain</h5></div>
+ </div>
+
+    </main>
+    <script src="js/observers.js"></script>
+  </body>
+</html>
+<style>
+
+
+</style>
---
+++ test.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>My Work</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet"  type="text/css"href="css/style.php">
+    <style>
+  /* Make the image fully responsive */
+  .carousel-inner img {
+    width: 100%;
+    height: 50%;
+  }
+  </style>
+</head>
+<body>
+<nav class="navbar navbar-expand-md  navbar-light bg-light  sticky-top">
+<div class="container-fluid">
+<a class="navbar-brand" href="index.php"><img src="st/lo.jpg" alt=""  width="50px" height="50px" /></a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+ <span class="nav-toggler-icon"></span>
+</button>
+
+<div class=" collapse navbar-collapse" id="navbarResponsive">
+<ul class="navbar-nav ml-auto">
+   <li class="nav-item  active">
+      <a class="nav-link" href="index.php">HOME</a>
+    </li>
+    <li class="nav-item ">
+      <a class="nav-link" href="#">Link</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link</a>
+    </li>
+  </ul>
+   </div>
+ </div>
+ </nav>
+ <h2 class="text-center text-primary">Some of  My Website & Managemant System</h2>
+ 
+<div class="container">
+<h3 class=" bg-secondary text-center text-white ">Ecommerce Website</h3>
+<div class="row">
+
+	 <div class="col-md-12"><iframe width="100%" height="500" src="http://www.shammabs.com/" frameborder="0" ></iframe></div>
+	  <div class="pl-5"><a href="http://www.shammabs.com/"><button class="bg-info text-white p-2"> For go to the site</button></a></div>
+	  <h4 class="text-justify">I design and develop this website for a beauty  Parlour .I didn't use any type of framework or CMS. To make  this website,I use row php ,HTML, CSS, jQuery & Bootstrap 4    </h4>
+	  
+	  </div>
+  
+  
+
+<br />
+ <h3 class=" bg-secondary text-center text-white ">WordPress Website</h3>
+<div class="row">
+
+	 <div class="col-md-12"><iframe width="100%" height="500" src="https://bdfdmn.000webhostapp.com/" frameborder="0" ></iframe></div>
+	  <div class="pl-5"><a href="https://bdfdmn.000webhostapp.com/"><button class="bg-info text-white p-2"> For go to the site</button></a></div>
+	  <h4 class="text-justify">I design and develop this website for a project .It  is a wordpress website. </h4>
+	  
+	  </div>
+  
+  
+
+<br />
+
+<h3 class=" bg-secondary text-center text-white ">School Website</h3>
+<div class="row">
+
+	 <div class="col-md-12"><iframe width="100%" height="500" src="http://smsst.000webhostapp.com/" frameborder="0" ></iframe></div>
+	  <div class="pl-5"><a href="http://smsst.000webhostapp.com/"><button class="bg-info text-white p-2"> For go to the site</button></a></div>
+	  <h4 class="text-justify">I didn't use any type of famwork or CMS  here. To make  this website,I used  php  ,HTML, CSS & Bootstrap 4   </h4>
+	  
+	  </div>
+<br />
+  
+<h3 class=" bg-dark text-center text-white ">School Managemant  System</h3><br />
+<div class="row">
+
+	 <div class="col-md-9"><iframe width="100%" height="315" src="http://smsst.000webhostapp.com/sec/soft/" frameborder="0" ></iframe></div>
+	  <div class="col-md-3"><div class="pl-5"><a href="http://smsst.000webhostapp.com/sec/soft/"><button class="bg-info text-white p-2"> For go to the site</button></a></div>
+	  <h4 class="text-justify">I didn't use any type of famwork or CMS  here. To make  this website,I used  php ,HTML, CSS & Bootstrap 4  
+</h4><h6>
+<br />username	:--shafayet
+<br />Emai:--shafayet133@gmail.com 
+<br />password	:--123456 </h6>
+	  
+	  </div>
+  
+  
+</div>
+<br />
+  
+<h3 class=" bg-dark text-center text-white "> Bakery Managemant  System</h3><br />
+<div class="row">
+
+	 <div class="col-md-9"><iframe width="100%" height="315" src="http://stfac.000webhostapp.com/sec/soft/" frameborder="0" ></iframe></div>
+	  <div class="col-md-3"><div class="pl-5"><a href="http://stfac.000webhostapp.com/sec/soft/"><button class="bg-info text-white p-2"> For go to the site</button></a></div>
+	  <h4 class="text-justify">I didn't use any type of famwork or CMS  here. To make  this website,I used  php ,HTML, CSS & Bootstrap 4  
+</h4><h6>
+<br />username	:--shafayet
+<br />Emai:--shafayet133@gmail.com 
+<br />password	:--123456 </h6>
+	  
+	  </div>
+  
+  
+</div>
+  
+</div>
+  <div class="container bg-dark ">
+ <div class="text-center text-white"> <h5>Power by &#169; S@yed shafayet hossain</h5></div>
+ </div>
+</body>
+</html>
